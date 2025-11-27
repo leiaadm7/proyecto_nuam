a819bd39a7386f54ad06742fa7fc8c4516855895
leiaadm7 <leia.donoso@inacapmail.cl>
2025-11-27 19:42:14 -0300

    .

--- calificaciones/api_views.py
+++ calificaciones/api_views.py
@@ -1,12 +1,10 @@
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
+from django.db import models 
 from .models import Calificacion, LogAuditoria
-from serializers import CalificacionSerializer, LogAuditoriaSerializer
+from .serializers import CalificacionSerializer, LogAuditoriaSerializer
 
-
-# --- PERMISOS POR ROL ---
 class EsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name="ADMIN").exists()
@@ -21,8 +19,6 @@ class EsAuditor(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name="AUDITOR").exists()
 
-
-# --- API PRINCIPAL ---
 class CalificacionViewSet(viewsets.ModelViewSet):
     queryset = Calificacion.objects.all().order_by("-fecha_registro")
     serializer_class = CalificacionSerializer
@@ -31,11 +27,11 @@ class CalificacionViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(analista=self.request.user)
 
-    # Endpoint para gráficos
     @action(detail=False, methods=["get"])
     def resumen_por_pais(self, request):
         data = (
-            Calificacion.objects.values("pais")
+            Calificacion.objects
+            .values("pais")
             .order_by("pais")
             .annotate(total=models.Count("id"))
         )
--- nuam_holding/urls.py
+++ nuam_holding/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from calificaciones import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
+    path('api/', include('calificaciones.api_urls')),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='calificaciones/login.html'), name='login'),
