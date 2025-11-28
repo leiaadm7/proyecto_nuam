870170b0f62c06ea1aafe43bd05104856d3b2ab1
leiaadm7 <leia.donoso@inacapmail.cl>
2025-11-28 03:26:14 -0300

    cambio historial

--- calificaciones/api_views.py
+++ calificaciones/api_views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.db import models 
 from .models import Calificacion, LogAuditoria
 from .serializers import CalificacionSerializer, LogAuditoriaSerializer
@@ -20,43 +21,76 @@ class EsAuditor(permissions.BasePermission):
 class CalificacionViewSet(viewsets.ModelViewSet):
     queryset = Calificacion.objects.all().order_by("-fecha_registro")
     serializer_class = CalificacionSerializer
-
     def get_permissions(self):
         if self.action == 'destroy':
             permission_classes = [EsAdmin]
-
         elif self.action in ['create', 'update', 'partial_update']:
             permission_classes = [EsAdmin | EsInterno]
-
         else:
             permission_classes = [permissions.IsAuthenticated]
-            
         return [permission() for permission in permission_classes]
 
-    # --- Auditoría Automática ---
+
     def perform_create(self, serializer):
         instancia = serializer.save(analista=self.request.user)
-        self.registrar_log("CREAR", f"Creó registro {instancia.pais} - {instancia.tipo}")
+        
+        detalle = (
+            f"NUEVO #{instancia.id} | "
+            f"País: {instancia.get_pais_display()} | "
+            f"Tipo: {instancia.get_tipo_display()} | "
+            f"Monto: ${instancia.monto_base} | "
+            f"Factor: {instancia.factor}"
+        )
+        self.registrar_log("CREAR", detalle)
 
     def perform_update(self, serializer):
         instancia = serializer.save()
-        self.registrar_log("EDITAR", f"Modificó registro #{instancia.id} ({instancia.pais})")
+        
+        detalle = (
+            f"EDITADO #{instancia.id} | "
+            f"País: {instancia.get_pais_display()} | "
+            f"Tipo: {instancia.get_tipo_display()} | "
+            f"Nuevo Monto: ${instancia.monto_base} | "
+            f"Nuevo Factor: {instancia.factor}"
+        )
+        self.registrar_log("EDITAR", detalle)
 
     def perform_destroy(self, instance):
-        self.registrar_log("ELIMINAR", f"Borró registro #{instance.id} ({instance.pais})")
+        detalle = (
+            f"ELIMINADO #{instance.id} | "
+            f"País: {instance.get_pais_display()} | "
+            f"Monto Perdido: ${instance.monto_base}"
+        )
+        self.registrar_log("ELIMINAR", detalle)
         instance.delete()
 
     def registrar_log(self, accion, detalle):
-        LogAuditoria.objects.create(usuario=self.request.user, accion=accion, detalle=detalle)
+        LogAuditoria.objects.create(
+            usuario=self.request.user, 
+            accion=accion, 
+            detalle=detalle
+        )
 
     @action(detail=False, methods=["get"])
     def resumen_por_pais(self, request):
         data = Calificacion.objects.values("pais").annotate(total=models.Count("id"))
         return Response(list(data))
 
-
 class LogAuditoriaViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = LogAuditoria.objects.all().order_by("-fecha_hora")
     serializer_class = LogAuditoriaSerializer
-
     permission_classes = [EsAdmin | EsAuditor]
+
+class UserInfoView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        grupo = "NINGUNO"
+        if request.user.groups.filter(name='ADMIN').exists():
+            grupo = "ADMIN"
+        elif request.user.groups.filter(name='INTERNO').exists():
+            grupo = "INTERNO"
+        elif request.user.groups.filter(name='AUDITOR').exists():
+            grupo = "AUDITOR"
+        
+        return Response({"username": request.user.username, "grupo": grupo})
--- calificaciones/views.py
+++ calificaciones/views.py
@@ -1,175 +1,104 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
 from .models import Calificacion, LogAuditoria
-from .forms import *
-from .utils import registrar_log
+from .forms import CalificacionForm, CargaMasivaForm
 import csv
 from io import TextIOWrapper
 
 def home(request):
-    """
-    Entra a / y redirige al login.
-    """
     return redirect('login')
 
 @login_required
 def dashboard(request):
-    """
-    Vista principal con la tabla de calificaciones.
-    """
     registros = Calificacion.objects.all().order_by('-fecha_registro')
     return render(request, 'calificaciones/dashboard.html', {'registros': registros})
 
+@login_required
+def historial(request):
+
+    if not request.user.has_perm('calificaciones.view_logauditoria'):
+        return redirect('dashboard')
+        
+    logs = LogAuditoria.objects.all().order_by('-fecha_hora')
+    return render(request, 'calificaciones/historial.html', {'logs': logs})
 
 @login_required
+@permission_required('calificaciones.add_calificacion', raise_exception=True)
 def crear_calificacion(request):
-    """
-    Crear una nueva calificación desde el formulario HTML.
-    """
     if request.method == 'POST':
         form = CalificacionForm(request.POST)
         if form.is_valid():
-            obj = form.save(commit=False)
-            obj.analista = request.user
-            obj.save()
+            instancia = form.save(commit=False)
+            instancia.analista = request.user
+            instancia.save()
 
-            # Auditoría
-            registrar_log(
+            LogAuditoria.objects.create(
                 usuario=request.user,
                 accion="CREAR",
-                detalle_dict={
-                    "pais": obj.pais,
-                    "tipo": obj.tipo,
-                    "monto_base": float(obj.monto_base),
-                    "factor": float(obj.factor)
-                }
+                detalle=f"Desde App Django: {instancia.pais} - {instancia.tipo}"
             )
-
-            messages.success(request, "Calificación creada correctamente.")
             return redirect('dashboard')
     else:
         form = CalificacionForm()
-
     return render(request, 'calificaciones/formulario.html', {'form': form})
 
-
 @login_required
+@permission_required('calificaciones.change_calificacion', raise_exception=True)
 def editar_calificacion(request, id):
-    """
-    Editar cualquier calificación existente.
-    """
     calificacion = get_object_or_404(Calificacion, id=id)
-
-    datos_antes = {
-        "pais": calificacion.pais,
-        "tipo": calificacion.tipo,
-        "monto_base": float(calificacion.monto_base),
-        "factor": float(calificacion.factor),
-    }
-
     if request.method == 'POST':
         form = CalificacionForm(request.POST, instance=calificacion)
         if form.is_valid():
-            nuevo_obj = form.save()
-
-            registrar_log(
+            form.save()
+            
+            LogAuditoria.objects.create(
                 usuario=request.user,
                 accion="EDITAR",
-                detalle_dict={
-                    "antes": datos_antes,
-                    "despues": {
-                        "pais": nuevo_obj.pais,
-                        "tipo": nuevo_obj.tipo,
-                        "monto_base": float(nuevo_obj.monto_base),
-                        "factor": float(nuevo_obj.factor)
-                    }
-                }
+                detalle=f"Desde App Django: Editó ID #{id}"
             )
-
-            messages.success(request, "Calificación actualizada correctamente.")
             return redirect('dashboard')
-
     else:
         form = CalificacionForm(instance=calificacion)
-
-    return render(request, 'calificaciones/formulario.html', {
-        'form': form,
-        'titulo': 'Editar Calificación'
-    })
-
+    return render(request, 'calificaciones/formulario.html', {'form': form, 'titulo': 'Editar Calificación'})
 
 @login_required
+@permission_required('calificaciones.delete_calificacion', raise_exception=True)
 def eliminar_calificacion(request, id):
-    """
-    Eliminar una calificación.
-    """
     calificacion = get_object_or_404(Calificacion, id=id)
-
-    registrar_log(
+    
+    LogAuditoria.objects.create(
         usuario=request.user,
         accion="ELIMINAR",
-        detalle_dict={
-            "id": calificacion.id,
-            "pais": calificacion.pais,
-            "tipo": calificacion.tipo
-        }
+        detalle=f"Desde App Django: Borró ID #{id} ({calificacion.pais})"
     )
-
+    
     calificacion.delete()
-    messages.success(request, "Calificación eliminada correctamente.")
     return redirect('dashboard')
 
-
 @login_required
+@permission_required('calificaciones.add_calificacion', raise_exception=True)
 def carga_masiva(request):
-    """
-    Subida masiva mediante CSV.
-    Campos esperados:
-    pais,tipo,monto,factor
-    """
     if request.method == 'POST':
         form = CargaMasivaForm(request.POST, request.FILES)
-
         if form.is_valid():
-
-            archivo = TextIOWrapper(
-                request.FILES['archivo_csv'].file,
-                encoding='utf-8'
-            )
-
+            archivo = TextIOWrapper(request.FILES['archivo_csv'].file, encoding='utf-8')
             csv_reader = csv.DictReader(archivo)
-
-            contador = 0
             for row in csv_reader:
                 Calificacion.objects.create(
-                    pais=row['pais'].strip().upper(),
+                    pais=row['pais'].strip().upper(),      
                     tipo=row['tipo'].strip().upper(),
-                    monto_base=float(row['monto']),
-                    factor=float(row['factor']),
+                    monto_base=row['monto'],
+                    factor=row['factor'],
                     analista=request.user
                 )
-                contador += 1
-
-            registrar_log(
-                usuario=request.user,
-                accion="CARGA MASIVA",
-                detalle_dict={"total_registros": contador}
+            
+            LogAuditoria.objects.create(
+                usuario=request.user, 
+                accion="CARGA MASIVA", 
+                detalle="Procesó archivo CSV desde App Django"
             )
-
-            messages.success(request, f"Carga masiva exitosa: {contador} registros.")
             return redirect('dashboard')
-
     else:
         form = CargaMasivaForm()
-
+    
     return render(request, 'calificaciones/carga_masiva.html', {'form': form})
-
-
-@login_required
-def historial(request):
-    """
-    Muestra el historial completo de acciones registradas.
-    """
-    logs = LogAuditoria.objects.all().order_by('-fecha_hora')
-    return render(request, 'calificaciones/historial.html', {'logs': logs})
