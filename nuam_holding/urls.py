f956e6895cb127c5cadcdcff55a359f87f6408da
leiaadm7 <leia.donoso@inacapmail.cl>
2025-12-04 02:43:00 -0300

    urls

--- nuam_holding/urls.py
+++ nuam_holding/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path('admin/', admin.site.urls),
     path('', include('calificaciones.urls')),
 ]
