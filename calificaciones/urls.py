d4ee7c70c826ad76ac9c4635454c169471597263
leiaadm7 <leia.donoso@inacapmail.cl>
2025-12-04 02:55:04 -0300

    urls

--- calificaciones/urls.py
+++ calificaciones/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(router.urls)),
-    path('', views.home, name='home'),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path('login/', auth_views.LoginView.as_view(template_name='calificaciones/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
--- nuam_holding/urls.py
+++ nuam_holding/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('', include('calificaciones.urls')),
 ]
